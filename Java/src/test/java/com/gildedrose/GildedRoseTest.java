2f316ebaec0c6466f2654bdb6e90ffef0bcd7fdd
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-02-22 22:08:09 -0500

    started working before i created the repo so i just deleted what i put in

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,12 +8,10 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 2, 7) };
-        System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        System.out.println("AFTER:  sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
-        assertEquals(9, app.items[0].sellIn);
+        assertEquals("fix me", app.items[0].name);
     }
     
     

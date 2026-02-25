55fb086bd9fc46b2d6ed5806f8dcf4103cb34d52
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-02-25 17:28:44 -0500

    Carried out tests on aged brie logic

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,15 +4,45 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void agedBrieQualityIncreasedBy1() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+        System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fix me", app.items[0].name);
+        System.out.println("AFTER:  sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
+        //System.out.println("AFTER:  name=" + items[0].name);
+        assertEquals(21, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
     }
+    
+    @Test
+    void agedBrieQualityNeverExceeds50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        System.out.println("AFTER:  sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
+        //System.out.println("AFTER:  name=" + items[0].name);
+        assertEquals(50, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
+    
+    @Test
+    void agedBrieQualityIncreasesBy2() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 44) };
+        System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        System.out.println("AFTER:  sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
+        //System.out.println("AFTER:  name=" + items[0].name);
+        assertEquals(46, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+    
     
     
 

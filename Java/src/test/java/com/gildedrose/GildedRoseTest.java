5211196524cc54e4cf49063cefc387a10c7859e0
steeve mocto <amoct@students.kennesaw.edu>
2026-02-26 12:35:52 -0500

    tests: test for quality decreases by one and never less than zero working fine.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,8 +13,18 @@ class GildedRoseTest {
 
         app.updateQuality();
 
-        assertEquals(19, app.items[0].quality);
+        assertEquals(19, app.items[0].quality, "Quality should decrease by 1 for normal items");
 
+    }
+
+    @Test
+    void testQualityNeverNegative() {
+        Item[] items = new Item[] { new Item("Normal Item", 10, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality, "Quality should never be negative");
     }
 
 

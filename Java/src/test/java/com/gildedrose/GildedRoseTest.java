f3c924b9d95f8dd3bb448751f5ca653c28e5c76c
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-02-26 19:14:27 -0500

    comments added to test functions

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,30 +8,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
+    //checks that aged-brie quality increases by 1
     void agedBrieQualityIncreasedBy1() {
         Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
-        System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        System.out.println("AFTER:  sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
-        //System.out.println("AFTER:  name=" + items[0].name);
         assertEquals(21, app.items[0].quality);
         assertEquals(9, app.items[0].sellIn);
     }
     
     @Test
+    //checks that aged-brie quality is capped at 50
     void agedBrieQualityNeverExceeds50() {
         Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
         System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        System.out.println("AFTER:  sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
-        //System.out.println("AFTER:  name=" + items[0].name);
         assertEquals(50, app.items[0].quality);
         assertEquals(9, app.items[0].sellIn);
     }
     
     @Test
+    //checks that aged-brie quality increases by 2 after sellin < 0
     void agedBrieQualityIncreasesBy2() {
         Item[] items = new Item[] { new Item("Aged Brie", 0, 44) };
         System.out.println("BEFORE: sellIn=" + items[0].sellIn + ", quality=" + items[0].quality);
